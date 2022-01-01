f10f68c9f0594dfc9fb95f6633ee8b2640a07fa7
Kashod Cagnolatti <kcagnolatti@callutheran.edu>
2021-12-31 17:25:21 -0800

    Update 1.0
    A basic drawing app based on Shuvo's Code, https://dev.to/0shuvo0/lets-create-a-drawing-app-with-js-4ej3 ,with some added functionality. Made to practice my CSS, HTML, and JavaScript. Plan to add more when I can.

---
+++ main.js
@@ -0,0 +1,70 @@
+const canvas = document.getElementById("canvas")
+canvas.height = window.innerHeight
+canvas.width = window.innerWidth
+
+const ctx = canvas.getContext("2d")
+
+let prevX = null
+let prevY = null
+
+ctx.lineWidth = 5
+document.getElementById("thick").value = ctx.lineWidth
+let draw = false
+
+// Draw Actions
+window.addEventListener("mousedown", (e) => draw = true)
+window.addEventListener("mouseup", (e) => draw = false)
+
+window.addEventListener("mousemove", (e) => {
+    if(prevX == null || prevY == null || !draw){
+        prevX = e.clientX
+        prevY = e.clientY
+        return
+    }
+
+    let currentX = e.clientX
+    let currentY = e.clientY
+
+    ctx.beginPath()
+    ctx.moveTo(prevX, prevY)
+    ctx.lineTo(currentX, currentY)
+    ctx.stroke()
+
+    prevX = currentX
+    prevY = currentY
+})
+
+// App Buttons
+//----- Colors
+let clrs = document.querySelectorAll(".clr")
+clrs = Array.from(clrs)
+clrs.forEach(clr => {
+    clr.addEventListener("click", () => {
+        ctx.strokeStyle = clr.dataset.clr
+    })
+})
+
+//---- Brush Thickness
+let thickBtn = document.querySelector(".thickBtn")
+thickBtn.addEventListener("click", () => {
+    let thickness = document.getElementById("thick").value
+    ctx.lineWidth = thickness
+})
+
+//---- Clear
+let clearBtn = document.querySelector(".clear")
+clearBtn.addEventListener("click", () => {
+    // Clearning the entire canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+})
+
+//---- Save
+let saveBtn = document.querySelector(".save")
+saveBtn.addEventListener("click", () => {
+    let data = canvas.toDataURL("imag/png")
+    let a = document.createElement("a")
+    a.href = data
+    a.download = "sketch.png"
+    a.click()
+})
+
